9a4a0807c9654faf762bc7845b70905610ef8b48
Quinn Walker <quinn.walker@developersinstitute.co.nz>
2022-12-05 11:39:11 +1300

    Created main page and added link icons

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -8,6 +11,30 @@ const Main = () => {
           <p className="uppercase text-sm tracking-widest text-gray-600">
             LET&#39;S BUILD SOMETHING TOGETHER
           </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&#39;m <span className="text-[#5651e5]"> Quinn</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Front-end web Developer</h1>
+          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+            I&#39;m a front-end web developer specializing in building (and
+            occasionally designing) exceptional digital experiences. Currently,
+            I&#39;m focused on building responsive front-end web applications
+            while learning back-end technologies.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
         </div>
       </div>
     </div>
