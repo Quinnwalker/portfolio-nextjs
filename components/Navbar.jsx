c3c476c9673e559178aad64cf4d2c070d6a1807d
Quinn Walker <quinn.walker@developersinstitute.co.nz>
2022-12-09 22:42:10 +1300

    Adjusted nav bar text to be visible on projects page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,28 @@ import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-
+import { useRouter } from "next/router";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState("#ecf0f3");
+  const [linkColor, setLinkColor] = useState("#1f2937");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (
+      router.asPath === "/property" ||
+      router.asPath === "/crypto" ||
+      router.asPath === "/netflix" ||
+      router.asPath === "/twitch"
+    ) {
+      setNavBg("transparent");
+      setLinkColor("#ecf0f3");
+    } else {
+      setNavBg("ecf0f3");
+      setLinkColor("ecf0f3");
+    }
+  }, [router]);
 
   const handleNav = () => {
     setNav(!nav);
@@ -26,6 +44,7 @@ const Navbar = () => {
 
   return (
     <div
+      style={{ backgroundcolor: `${navBg}` }}
       className={
         shadow
           ? "fixed w-full h-20 shadow-xl z-[100]"
@@ -43,7 +62,7 @@ const Navbar = () => {
         </Link>
 
         <div>
-          <ul className="hidden md:flex">
+          <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
             <Link href="/">
               <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
